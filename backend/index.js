40268b004a129c59875582794060906fe71fdcb2
Anis-Jemail <jemail.anis@gmail.com>
2023-12-09 15:07:24 +0100

    Update backend and frontend configurations

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-require('dotenv').config(); // Load environment variables from .env file
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 7000; // Use environment variable PORT or default to 7000
+const PORT = process.env.PORT || 7000;
 
-// Example route
+app.use(cors()); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
   res.send('Hello from your Express server!');
 });
