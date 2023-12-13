4ff91b2582e8c3710aa23b4d593d08a731b570af
tarekHaker <tarekbnzaied@gmail.com>
2023-12-13 20:13:00 +0400

    backend login register

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  address: { type: String, required: true },
+  // address: { type: String, required: true },
  
 
 });
