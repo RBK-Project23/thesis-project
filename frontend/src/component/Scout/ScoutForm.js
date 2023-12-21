4e18c023cb737fa3984852189093205fdc694a7e
Anis-Jemail <jemail.anis@gmail.com>
2023-12-21 19:59:11 +0100

    correct path of axios

--- frontend/src/component/Scout/ScoutForm.js
+++ frontend/src/component/Scout/ScoutForm.js
@@ -37,7 +37,7 @@ const ScoutForm = () => {
     console.log('Submitting Scout:', scout);
   
     try {
-      const response = await axios.post('http://localhost:3000/scouts', scout, {
+      const response = await axios.post('http://localhost:7000/scouts', scout, {
         
        headers: {
         'Content-Type': 'application/json',
