010bbc09f4811598ccad6e8ef9620847c1f03ab0
tarekHaker <tarekbnzaied@gmail.com>
2023-12-22 12:41:46 +0400

    home page

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import homeImage from '../images/home.jpg';
+import '../component/homePage.css';
+import Footer from '../component/Scout/footer'
+
+
 
 const Home = () => {
-  return <div> WELCOME </div>;
+  return <> 
+  
+  <img id='home-image' src={homeImage} alt="Home" />
+  <span className="imageText">  مرحبا بكم بالكشافة التونسية بسلطنة عمان</span>
+
+  <Footer />
+  </>;
 };
 
 export default Home;
