6e8f90a5631b0706e55e23e902db49156a0c94a4
tarekHaker <tarekbnzaied@gmail.com>
2024-01-12 12:33:15 +0400

    update

--- frontend/src/component/usertable.js
+++ frontend/src/component/usertable.js
@@ -1,40 +1,48 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Link from "@mui/material/Link";
 
-
-
-
-
-
-
-
-
-export default function DenseTable({users, deleteUser,updateStatus}) {
-  const status = (email,status)=>{
-    if(status){return (
-      <Button variant="contained" color="success" size = 'small' onClick={() => updateStatus(email,status)} >
-      confirmed
-    </Button>
-    )}
-    else {return (
-      <Button variant="outlined" color="error" size = 'small'  onClick={() => updateStatus(email,status)}
-              
-              >pendding</Button>
-
-    )}
-  }
+export default function DenseTable({ users, deleteUser, updateStatus }) {
+  const status = (email, status) => {
+    if (status) {
+      return (
+        <Button
+          variant="contained"
+          color="success"
+          size="small"
+          onClick={() => updateStatus(email, status)}
+        >
+          confirmed
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          variant="outlined"
+          color="error"
+          size="small"
+          onClick={() => updateStatus(email, status)}
+        >
+          pendding
+        </Button>
+      );
+    }
+  };
   return (
-    <TableContainer component={Paper} style={{background:'transparent', marginTop:'0vh', color:'black'}}>
-     <h2 style={{textAlign:'center', padding:'1vh'}}> User list</h2>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table" >
+    <TableContainer
+      component={Paper}
+      style={{ background: "transparent", marginTop: "0vh", color: "black" }}
+    >
+      <h2 style={{ textAlign: "center", padding: "1vh" }}> Users list</h2>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             <TableCell
@@ -83,36 +91,60 @@ export default function DenseTable({users, deleteUser,updateStatus}) {
         </TableHead>
 
         <TableBody>
-          {currentItems.map((user) => (
+          {users.map((user) => (
             <TableRow
               key={user.firstNme}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row" style={{ color: "black" }}>
                 {user.firstName}
               </TableCell>
-              <TableCell align="right" style={{ color:'black'}}>{user.lastName}</TableCell>
-              <TableCell align="right" style={{ color:'black'}}>{user.email}</TableCell>
-              <TableCell align="right" style={{ color:'black'}}>{user.type_user}</TableCell>
-              <TableCell align="right" style={{ color:'black'}}>{status(user.email, user.status)}</TableCell>
-                  <TableCell align="right" style={{ color:'black'}}>
-              <Button variant="outlined"  startIcon={<DeleteIcon />} size = 'small' onClick={() => deleteUser(user.id)}
-              
-              >Delete</Button></TableCell>
+              <TableCell  style={{ color: "black" }}>
+                {user.lastName}
+              </TableCell>
+              <TableCell  style={{ color: "black" }}>
+                {user.email}
+              </TableCell>
+              <TableCell  style={{ color: "black" }}>
+                {user.type_user}
+              </TableCell>
+              <TableCell align="centre" style={{ color: "black" }}>
+                {status(user.email, user.status)}
+              </TableCell>
+              <TableCell align="center" style={{ color: "black" }}>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  component={Link}
+                  href="http://localhost:3000/doc-istimara"
+                  target="_blank"
+                >
+                  Istimara
+                </Button>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  component={Link}
+                  href="http://localhost:3000/Engagement"
+                  target="_blank"
+                >
+                  Engagement
+                </Button>
+              </TableCell>
+              <TableCell align="centre" style={{ color: "black" }}>
+                <Button
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  size="small"
+                  onClick={() => deleteUser(user._id)}
+                >
+                  Delete
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <div className='pages'>
-      {pageNumbers.map(number => (
-    <button className='page' key={number} onClick={() => setCurrentPage(number)}>
-      {number}
-    </button>
-  ))}
-      </div>
- 
     </TableContainer>
-    
   );
- 
 }
